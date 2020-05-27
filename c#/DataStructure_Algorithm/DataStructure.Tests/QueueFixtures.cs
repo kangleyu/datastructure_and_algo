c290780901da58d1def38f1678f113766e50c9ce
Tom Yu <kangleyu@gmail.com>
2020-05-27 19:39:22 +0800

    Add Queue implementation based on Array

---
+++ c#/DataStructure_Algorithm/DataStructure.Tests/QueueFixtures.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace DataStructure.Tests
+{
+    [TestFixture]
+    public class QueueFixtures
+    {
+        [Test]
+        public void Initialize_New_Queue_Should_Return_Empty_Queue()
+        {
+            IQueue<int> queue = new Queue<int>();
+            Assert.IsTrue(queue.IsEmpty());
+        }
+
+        [Test]
+        public void Add_Item_Into_Queue_Should_Be_Able_To_Peek_And_Popup()
+        {
+            IQueue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            Assert.AreEqual(5, queue.Count);
+            Assert.AreEqual(1, queue.Peek());
+            Assert.AreEqual(1, queue.Dequeue());
+            Assert.AreEqual(2, queue.Dequeue());
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+            queue.Enqueue(9);
+            queue.Enqueue(10);
+            queue.Enqueue(11);
+            queue.Enqueue(12);
+            queue.Enqueue(13);
+            queue.Enqueue(14);
+            queue.Enqueue(15);
+            queue.Enqueue(16);
+            queue.Enqueue(17);
+            Assert.AreEqual(3, queue.Dequeue());
+            Assert.AreEqual(4, queue.Dequeue());
+            Assert.AreEqual(5, queue.Dequeue());
+            Assert.AreEqual(6, queue.Dequeue());
+            Assert.AreEqual(7, queue.Dequeue());
+            Assert.AreEqual(8, queue.Dequeue());
+            Assert.AreEqual(9, queue.Dequeue());
+            Assert.AreEqual(10, queue.Dequeue());
+            Assert.AreEqual(11, queue.Dequeue());
+            Assert.AreEqual(12, queue.Dequeue());
+            Assert.AreEqual(13, queue.Dequeue());
+            Assert.AreEqual(14, queue.Dequeue());
+            Assert.AreEqual(15, queue.Dequeue());
+            Assert.AreEqual(16, queue.Dequeue());
+            Assert.AreEqual(17, queue.Dequeue());
+        }
+    }
+}
--- c#/DataStructure_Algorithm/DataStructure/Queue.cs
+++ c#/DataStructure_Algorithm/DataStructure/Queue.cs
@@ -1,12 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructure
 {
-    class Queue
+    /// <summary>
+    /// Queue interface
+    /// </summary>
+    /// <typeparam name="T">Queue Element</typeparam>
+    public interface IQueue<T>
     {
+        /// <summary>
+        /// Push the item into the queue
+        /// </summary>
+        /// <param name="value"></param>
+        void Enqueue(T value);
+
+        /// <summary>
+        /// Pop up the head of the queue
+        /// </summary>
+        /// <returns></returns>
+        T Dequeue();
+
+        /// <summary>
+        /// Return the head of the queue without removing
+        /// </summary>
+        /// <returns></returns>
+        T Peek();
+
+        /// <summary>
+        /// Represent whether queue is empty
+        /// </summary>
+        /// <returns></returns>
+        bool IsEmpty();
+
+        /// <summary>
+        /// Elements count in the queue
+        /// </summary>
+        int Count { get; }
+    }
+
+    /// <summary>
+    /// Queue implementation based on Array
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Queue<T> : IQueue<T>
+    {
+        private const int DEFAULT_CAPACITY = 4;
+
+        private T[] _internal;
+        private int _capaticy;
+        private int _head;
+        private int _tail;
+
+        public Queue()
+        {
+            _capaticy = DEFAULT_CAPACITY;
+            _internal = new T[DEFAULT_CAPACITY];
+            _head = 0;
+            _tail = -1;
+        }
+
+        public int Count { get; private set; }
+
+        public T Dequeue()
+        {
+            T head = Peek();
+            _head = (_head + 1) % _capaticy;
+            Count--;
+            return head;
+        }
+
+        public void Enqueue(T value)
+        {
+            int _available = _capaticy;
+
+            if (Count < _available)
+            {
+                _tail = (_tail + 1) % _available;
+            }
+            else if(Count == _available)
+            {
+                _capaticy = _capaticy * 2;
+                T[] copy_internal = new T[_capaticy];
+                for (int i = 0; i < Count; i++)
+                {
+
+                    int idx = (_head + i) % _available;
+                    copy_internal[i] = _internal[idx];
+                }
+                _internal = copy_internal;
+                _head = 0;
+                _tail = Count;
+            }
+
+            Count++;
+            _internal[_tail] = value;
+        }
+
+        public bool IsEmpty() => Count == 0;
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Empty queue");
+            }
+
+            return _internal[_head];
+        }
     }
 }
